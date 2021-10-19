7fa41dbf472e0ac74e75ff44c4b91afc167f1d89
jenny <913948843@qq.com>
2021-10-19 14:27:18 +0800

    first commit

---
+++ src/main/java/com/jenny/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.jenny.git;
+
+/**
+ * @author: jenny
+ * @create: 2021-10-19 2:12 下午
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+        System.out.println("hello git~");
+    }
+}
